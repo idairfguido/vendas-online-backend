bb4aca27818c35b3cab688c944a5e50aa10e6bb7
Idair F. Guido <idair_guido@hotmail.com>
2023-02-15 15:00:34 -0300

    Implementação

---
+++ src/user/user.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('user')
+export class UserController {
+
+    @Get()
+    async getAllUsers(){
+       return JSON.stringify({test: 'abc'});
+    }
+
+}
