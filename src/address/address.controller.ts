3b59047d2618852d0a68ef920bf88064a22410f7
Idair F. Guido <idair_guido@hotmail.com>
2023-02-20 14:44:41 -0300

    feat: insert controller address

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AddressService } from './address.service';
+import { CreateAddressDto } from './dtos/createAddress.dto';
+import { AddressEntity } from './entities/address.entity';
 
 @Controller('address')
-export class AddressController {}
+export class AddressController {
+  constructor(private readonly addressService: AddressService) {}
+
+  @Post('/:userId')
+  @UsePipes(ValidationPipe)
+  async createAddress(
+    @Body() createAddressDto: CreateAddressDto,
+    @Param('userId') userId: number,
+  ): Promise<AddressEntity> {
+    return this.addressService.createAddress(createAddressDto, userId);
+  }
+}
--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AddressService } from './address.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AddressController } from './address.controller';
+import { AddressService } from './address.service';
+import { AddressEntity } from './entities/address.entity';
 
+@Module({})
 @Module({
+  imports: [TypeOrmModule.forFeature([AddressEntity])],
+  controllers: [AddressController],
   providers: [AddressService],
-  controllers: [AddressController]
 })
 export class AddressModule {}
