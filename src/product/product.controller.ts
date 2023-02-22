75e848cf1d805b9d8f34f64272615f6b3a1fd534
Idair F. Guido <idair_guido@hotmail.com>
2023-02-22 14:19:13 -0300

    feat: create productService

---
+++ src/product/product.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from '@nestjs/common';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserType } from 'src/user/enum/user-type.enum';
+import { ReturnProductDto } from './dtos/return-product.dto';
+import { ProductService } from './product.service';
+
+@Roles(UserType.Admin, UserType.User)
+@Controller('product')
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Get()
+  async findAll(): Promise<ReturnProductDto[]> {
+    return (await this.productService.findAll()).map(
+      (product) => new ReturnProductDto(product),
+    );
+  }
+}
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { ProductController } from './product.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductEntity } from './entities/product.entity';
 
 @Module({
-  providers: [],
-  controllers: []
+  imports: [TypeOrmModule.forFeature([ProductEntity])],
+  providers: [ProductService],
+  controllers: [ProductController],
 })
 export class ProductModule {}
