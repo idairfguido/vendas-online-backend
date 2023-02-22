5e2bae421197f04f1921bb1634abe4e65ff011d2
Idair F. Guido <idair_guido@hotmail.com>
2023-02-22 17:56:48 -0300

    feat: create function update product

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -5,7 +5,6 @@ import { ProductEntity } from './entities/product.entity';
 import { CategoryService } from '../category/category.service';
 import { CreateProductDto } from './dtos/create-product.dto';
 import { UpdateProductDto } from './dtos/update-product.dto';
-import { runInThisContext } from 'vm';
 
 @Injectable()
 export class ProductService {
