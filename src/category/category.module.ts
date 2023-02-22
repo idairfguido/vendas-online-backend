1236d19d9677cedc41eeb2bcd8838ec0fa7acae8
Idair F. Guido <idair_guido@hotmail.com>
2023-02-22 10:46:44 -0300

    feat: create categoryService

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryEntity } from './entities/category.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CategoryEntity])],
   providers: [CategoryService],
   controllers: [CategoryController]
 })
