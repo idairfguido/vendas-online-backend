6b3d515cdac02e9e6b290a17b930de92c2992af4
Idair F. Guido <idair_guido@hotmail.com>
2023-02-22 16:15:56 -0300

    feat: insert function delete product

--- src/product/__tests__/product.service.spec.ts
+++ src/product/__tests__/product.service.spec.ts
@@ -7,6 +7,7 @@ import { ProductService } from '../product.service';
 import { productMock } from '../__mocks__/product.mock';
 import { categoryMock } from '../../category/__mocks__/category.mock';
 import { createProductMock } from '../__mocks__/create-product.mock';
+import { returnDeleteMock } from '../__mocks__/return-delete.mock';
 
 describe('ProductService', () => {
   let service: ProductService;
@@ -27,7 +28,9 @@ describe('ProductService', () => {
           provide: getRepositoryToken(ProductEntity),
           useValue: {
             find: jest.fn().mockResolvedValue([productMock]),
+            findOne: jest.fn().mockResolvedValue(productMock),
             save: jest.fn().mockResolvedValue(productMock),
+            delete: jest.fn().mockResolvedValue(returnDeleteMock),
           },
         },
       ],
@@ -70,10 +73,28 @@ describe('ProductService', () => {
     expect(product).toEqual(productMock);
   });
 
-  it('should return product after insert in DB', async () => {
+  it('should return error in exception before insert product in DB', async () => {
     jest.spyOn(categoryService, 'findCategoryById').mockRejectedValue(new Error());
 
     expect(service.createProduct(createProductMock)).rejects.toThrowError();
+  });
+
+  it('should return product in find by id', async () => {
+    const product = await service.findProductById(productMock.id);
+
+    expect(product).toEqual(productMock);
+  });
+
+  it('should return error in product not found', async () => {
+    jest.spyOn(productRepository, 'findOne').mockResolvedValue(undefined);
+
+    expect(service.findProductById(productMock.id)).rejects.toThrowError();
+  });
+
+  it('should return deleted true in delete product', async () => {
+    const deleted = await service.deleteProduct(productMock.id);
+
+    expect(deleted).toEqual(returnDeleteMock);
   });
   
 });
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
-import { Roles } from 'src/decorators/roles.decorator';
-import { UserType } from 'src/user/enum/user-type.enum';
+import { Body, Controller, Delete, Get, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Roles } from '../decorators/roles.decorator';
+import { UserType } from '../user/enum/user-type.enum';
+import { DeleteResult } from 'typeorm';
 import { CreateProductDto } from './dtos/create-product.dto';
 import { ReturnProductDto } from './dtos/return-product.dto';
 import { ProductEntity } from './entities/product.entity';
@@ -25,5 +26,13 @@ export class ProductController {
     @Body() createProductDto: CreateProductDto
   ):Promise<ProductEntity>{
     return this.productService.createProduct(createProductDto);
+  }
+
+  @Roles(UserType.Admin)
+  @Delete('/:productId')
+  async deleteProduct(
+    @Param('productId') productId: number,
+  ): Promise<DeleteResult> {
+    return this.productService.deleteProduct(productId);
   }
 }
