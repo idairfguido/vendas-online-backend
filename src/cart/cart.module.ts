9686060b05571b7c2838de5c8dafdd9db66280ea
Idair F. Guido <idair_guido@hotmail.com>
2023-02-23 14:54:56 -0300

    feat: create cart_product entity

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CartService } from './cart.service';
 import { CartController } from './cart.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CartEntity } from './entities/cart.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CartEntity])],
   providers: [CartService],
   controllers: [CartController]
 })
-export class CartModule {}
+export class CartModule { }
