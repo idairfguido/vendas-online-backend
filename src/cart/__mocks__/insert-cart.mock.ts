43fa324dffa3b2abb21640bbce175a05b4ac7b36
Idair F. Guido <idair_guido@hotmail.com>
2023-02-24 10:59:57 -0300

    feat: create unit test verifyProductInCart and createProductInCart

---
+++ src/cart/__mocks__/insert-cart.mock.ts
@@ -0,0 +1,8 @@
+import { productMock } from '../../product/__mocks__/product.mock';
+import { InsertCartDto } from '../dtos/insert-cart.dto';
+
+
+export const insertCartMock: InsertCartDto = {
+    amount: 535,
+    productId: productMock.id,
+};
